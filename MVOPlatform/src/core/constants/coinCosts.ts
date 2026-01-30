5e48a8ee51049b4c4aeb0206ed1d68694fbd6ca2
Alejandro Jose De Oliveira Barrancos <alejandrojose.deoliveira@gmail.com>
2026-01-30 19:11:48 -0400

    Add payments

--- MVOPlatform/src/core/constants/coinCosts.ts
+++ MVOPlatform/src/core/constants/coinCosts.ts
@@ -3,10 +3,10 @@
  * Use these constants everywhere that spends or displays coin costs.
  */
 export const PERSONA_PANEL = 10
-export const DEEP_RESEARCH = 30
-export const FULL_SYNTHESIS = 40
-export const RE_RUN = 10
 export const RISK_HIGHLIGHTER = 10
+export const DEEP_RESEARCH = 40
+export const FULL_SYNTHESIS = 30
+export const RE_RUN = 10
 
 /** Coin packages added on purchase (Starter / Builder / Operator) */
 export const PLAN_COINS = {
